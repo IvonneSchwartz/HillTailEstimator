9f3edf5a014f89b485ceec4fa8b812253c0c1000
Ivonne <7093076+luettedeern@users.noreply.github.com>
2017-12-22 14:30:05 +0100

    Update hill estimator

--- hill_estimator.jl
+++ hill_estimator.jl
@@ -3,15 +3,27 @@
 # ------------------------------------------------------
 # HILL TAIL ESTIMATOR
 # ------------------------------------------------------
-
+#
 
 function hill(x::Vector{Float64})
     lx=length(x)
     x=sort(abs.(x))
-    ly=Int(round(0.1*lx)) # fixed threshold of 10% 
+    ly=Int(round(0.1*lx)) # fixed threshold of 10%
     z=Vector{Float64}(ly)
     for k = 1 : ly
         z[k]=1/((1/k)*sum(log(x[lx-i+1])-log(x[lx-k]) for i in 1:k))
     end
     return z
+end
+
+function hill(x::Vector{Float64},y::Float64)
+    lx=length(x)
+    x=sort(abs.(x))
+    ly=Int(round(0.1*lx)) # fixed threshold of 10%
+    z=Vector{Float64}(ly)
+    lz=Int(round(y*lx))
+    for k = 1 : ly
+        z[k]=1/((1/k)*sum(log(x[lx-i+1])-log(x[lx-k]) for i in 1:k))
+    end
+    return z[lz]
 end
